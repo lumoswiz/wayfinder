b26ffee2d539612f8313837d7d931df77c1c8f91
lumoswiz <lumoswiz@protonmail.com>
2025-10-03 12:32:03 +0200

    refactor: remove holdings from world state

--- src/engine.rs
+++ src/engine.rs
@@ -30,22 +30,11 @@ impl<'a, P: Pool> Engine<'a, P> {
         Self { pools }
     }
 
-    pub fn simulate_chained(
-        &self,
-        world: &World<P::State>,
-        plan: &[Hop],
-        first_in: U256,
-        cap_first_hop: bool,
-    ) -> Path {
+    pub fn simulate_chained(&self, world: &World<P::State>, plan: &[Hop], first_in: U256) -> Path {
         assert!(!plan.is_empty(), "path must have at least one hop");
 
         let start_token = plan[0].1;
-        let mut amt_in = if cap_first_hop {
-            let have = *world.holdings.get(&start_token).unwrap_or(&U256::ZERO);
-            first_in.min(have)
-        } else {
-            first_in
-        };
+        let mut amt_in = first_in;
 
         let mut scratch: HashMap<PoolId, P::State> = HashMap::new();
 
@@ -89,95 +78,5 @@ impl<'a, P: Pool> Engine<'a, P> {
         }
 
         Path { steps }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use alloy_primitives::U256;
-
-    #[derive(Clone, Debug, Default, PartialEq, Eq)]
-    struct UnitState;
-
-    #[derive(Clone, Debug)]
-    struct FeePool {
-        id_: PoolId,
-        a: TokenId,
-        b: TokenId,
-        fee_bps: u32,
-    }
-
-    impl Pool for FeePool {
-        type State = UnitState;
-
-        fn id(&self) -> PoolId {
-            self.id_
-        }
-
-        fn supports(&self, from: TokenId, to: TokenId) -> bool {
-            (from == self.a && to == self.b) || (from == self.b && to == self.a)
-        }
-
-        fn swap(&self, _st: &mut Self::State, _from: TokenId, _to: TokenId, amt_in: U256) -> U256 {
-            let num = U256::from(10_000u32 - self.fee_bps);
-            (amt_in * num) / U256::from(10_000u32)
-        }
-    }
-
-    #[test]
-    fn chained_simulation() {
-        let x = TokenId(1);
-        let y = TokenId(2);
-        let z = TokenId(3);
-        let p_xy = PoolId(10);
-        let p_yz = PoolId(11);
-
-        let mut pools: HashMap<PoolId, FeePool> = HashMap::new();
-        pools.insert(
-            p_xy,
-            FeePool {
-                id_: p_xy,
-                a: x,
-                b: y,
-                fee_bps: 100,
-            },
-        );
-        pools.insert(
-            p_yz,
-            FeePool {
-                id_: p_yz,
-                a: y,
-                b: z,
-                fee_bps: 200,
-            },
-        );
-
-        let engine = Engine::new(&pools);
-
-        let mut world: World<UnitState> = World::default();
-        world.pool_states.insert(p_xy, UnitState::default());
-        world.pool_states.insert(p_yz, UnitState::default());
-        world.holdings.insert(x, U256::from(1_000_000u64));
-        world.holdings.insert(y, U256::ZERO);
-        world.holdings.insert(z, U256::ZERO);
-
-        let world_before = world.clone();
-
-        let plan: &[Hop] = &[(p_xy, x, y), (p_yz, y, z)];
-        let first_in = U256::from(250_000u64);
-
-        let path = engine.simulate_chained(&world, plan, first_in, true);
-
-        assert_eq!(world.pool_states, world_before.pool_states);
-        assert_eq!(world.holdings, world_before.holdings);
-
-        assert_eq!(path.steps.len(), 2);
-        assert_eq!(path.steps[0].amt_in, U256::from(250_000u64));
-        assert_eq!(path.steps[0].amt_out, U256::from(247_500u64));
-        assert_eq!(path.steps[1].amt_in, path.steps[0].amt_out);
-        assert_eq!(path.steps[1].amt_out, U256::from(242_550u64));
-
-        assert_eq!(path.steps[1].to, z);
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -1,9 +1,7 @@
-use crate::ids::{PoolId, TokenId};
-use alloy_primitives::U256;
+use crate::ids::PoolId;
 use std::collections::HashMap;
 
 #[derive(Clone, Debug, Default)]
 pub struct World<S> {
-    pub holdings: HashMap<TokenId, U256>,
     pub pool_states: HashMap<PoolId, S>,
 }
